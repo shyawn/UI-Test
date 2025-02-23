ec1b9f9aa6738b1c4a9a72f432b9f6fe53fc0ccc
ShawnCsj <csjshawn@gmail.com>
2025-02-23 10:39:48 +0800

    chore: edit header

--- App.tsx
+++ App.tsx
@@ -1,16 +1,68 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, FlatList, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ScrollView,
+  Image,
+} from 'react-native';
 // import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-// import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import Home from './src/pages/Home';
 
 // const Tab = createBottomTabNavigator();
 
+const Stack = createNativeStackNavigator();
+
 function App(): React.JSX.Element {
   return (
-    <View>
-      <Home />
-    </View>
+    // <View>
+    //   <Home />
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: true}}>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: null,
+            headerTitle: () => (
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  paddingLeft: 20,
+                }}>
+                <Image
+                  style={{marginLeft: 80}}
+                  source={require('../app/src/assets/logo.png')}
+                />
+                <Image
+                  style={{width: 22, height: 22}}
+                  source={require('../app/src/assets/notification.png')}
+                />
+              </View>
+            ),
+
+            headerRight: () => (
+              <View style={{flexDirection: 'row'}}>
+                {/* <TouchableOpacity style={styles.signinheader} onPress={() => navigation.navigate('Home')}>
+                <Text style={styles.signincolor}>SIGN IN</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={{marginLeft:10}} onPress={() => navigation.navigate('Home')}>
+                <Text style={styles.signincolor}>Press Me</Text>
+              </TouchableOpacity> */}
+              </View>
+            ),
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
     // <NavigationContainer>
     //   <Tab.Navigator
     //   // screenOptions={({route}) => ({
